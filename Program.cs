301522c1bc7d9188c400fa4d19c18f2f0b627872
Tatiana Tcyganova <tatiana.kv@gmail.com>
2023-07-13 19:35:28 +0300

    New excersises

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача 25:
+// Console.Write("Введите число a: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число b: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// int result =  Convert.ToInt32(Math.Pow(a, b)) ;
+// Console.WriteLine($" Число {a} в натуральной степени {b} = {result}");
+
+// //Задача 27:
+// Console.Write("Введите любое число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
+// while (n != 0)
+// {
+// sum = sum + n % 10;
+// n = n / 10;
+// }
+// Console.WriteLine($"Сумма цифр в числе = {sum}");
+
+// // Задача 29:
+// Console.WriteLine("Будет создан массив длиной 8 в диапазоне 10-1000");
+// int[] array = new int[8];
+// for (int i = 0; i < 8; i++)
+// {
+// array[i] = new Random().Next (10, 1000);
+// }
+// Console.WriteLine($"Массив: [ {string.Join("; ", array)} ]");
